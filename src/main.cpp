c8170c22b1ea42891d91ab24d5556610d0ffed06
andi-frame <andifarhan1094@gmail.com>
2025-04-08 16:07:50 +0700

    style

--- src/main.cpp
+++ src/main.cpp
@@ -2,37 +2,39 @@
 using namespace std;
 
 int main() {
-  int method, minBlockSize;
-  double threshold, percentageTarget;
-  string inputPath, outputPath, outputGifPath;
+    int method, minBlockSize;
+    double threshold, percentageTarget;
+    string inputPath, outputPath, outputGifPath;
 
-  cout << "Input absolute image path: ";
-  cin >> inputPath;
+    cout << "Input absolute image path: ";
+    cin >> inputPath;
 
-  cout << "Error Measurement Methods:";
-  cout << "1. Variance";
-  cout << "2. Mean Absolute Deviation (MAD)";
-  cout << "3. Max Pixel Difference";
-  cout << "4. Entropy";
-  cout << "5. Structural Similarity Index (SSIM)";
-  cout << "Input choosen method (number only): ";
-  cin >> method;
+    cout << "Error Measurement Methods:";
+    cout << "1. Variance";
+    cout << "2. Mean Absolute Deviation (MAD)";
+    cout << "3. Max Pixel Difference";
+    cout << "4. Entropy";
+    cout << "5. Structural Similarity Index (SSIM)";
+    cout << "Input choosen method (number only): ";
+    cin >> method;
 
-  cout << "Input Threshold: ";
-  cin >> threshold;
+    cout << "Input Threshold: ";
+    cin >> threshold;
 
-  cout << "Input minimum block size: ";
-  cin >> minBlockSize;
+    cout << "Input minimum block size: ";
+    cin >> minBlockSize;
 
-  cout << "Input target compression percentage in floating number (1.0 = 100%) (0 to disable): " << endl;
-  cin >> percentageTarget;
+    cout << "Input target compression percentage in floating number (1.0 = "
+            "100%) "
+            "(0 to disable): "
+         << endl;
+    cin >> percentageTarget;
 
-  cout << "Input absolute result image path: ";
-  cin >> outputPath;
+    cout << "Input absolute result image path: ";
+    cin >> outputPath;
 
-  cout << "Input absolute result gif path: ";
-  cin >> outputGifPath;
+    cout << "Input absolute result gif path: ";
+    cin >> outputGifPath;
 
-
-  return 0;
+    return 0;
 }
